8bfef0adc2e522f70c860961dfe4264f60ae49b0
Daniil <up201706117@up.pt>
2021-12-10 21:00:45 +0000

    updated criar.sql added table doctor and minor changes

--- criar.sql
+++ criar.sql
@@ -11,53 +11,6 @@ DROP TABLE IF EXISTS INMATE;
 DROP TABLE IF EXISTS TEAM_SUPERVISOR;
 DROP TABLE IF EXISTS VISITS;
 DROP TABLE IF EXISTS CRIME;
-
-CREATE TABLE VISIT(
-
-    visitDate DATE,
-    visitorId INTEGER,
-    relationToInmate TEXT,
-    PRIMARY KEY (visitDate,visitorId)
-
-);
-CREATE TABLE ASSIGNED_DOCTOR(
-    staffID INTEGER,
-    blockID INTEGER,
-
-    FOREIGN KEY (staffId) REFERENCES DOCTOR(staffId),    
-    FOREIGN KEY (blockId) REFERENCES PRISON_BLOCK(blockId)
-   
-);
-CREATE TABLE MEDICAL_APPOINTMENT(
-    appointmentDate DATE,
-    reason TEXT,
-    staffID INTEGER,
-    inmateId INTEGER,
-    FOREIGN KEY (staffId) REFERENCES DOCTOR(staffId),
-    FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId)
-);
-CREATE TABLE SENTENCE(
-    inmateId INTEGER,
-    crimeId INTEGER,
-    
-    duration INTEGER,
-    startingDate DATE,
-    --releaseDate AS startingDate + duration DATE,
-
-    FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId),
-    FOREIGN KEY (crimeId) REFERENCES CRIME(crimeId)
-
-    /*severity CASE
-        WHEN duration<=5 THEN 1
-        WHEN duration>5 AND duration<=10 THEN 2
-        WHEN duration>10 AND duration<=20 THEN 3
-        WHEN duration>20 AND duration<=40 THEN 4
-        ELSE 5
-    END as INTEGER;
-    */
-    
-
-);
 CREATE TABLE STAFF(
     staffId INTEGER PRIMARY KEY NOT NULL,
     staffName TEXT,
@@ -72,6 +25,12 @@ CREATE TABLE GUARD(
     assignedTo INTEGER,
     FOREIGN KEY (guardId) REFERENCES STAFF(staffId),
     FOREIGN KEY (assignedTo) REFERENCES CORRIDOR(corridorId)
+);
+CREATE TABLE DOCTOR(
+    doctorId INTEGER PRIMARY KEY NOT NULL,
+    licenseNumber INTEGER UNIQUE NOT NULL,
+    speciality TEXT,
+    FOREIGN KEY (doctorId) REFERENCES STAFF(staffId)
 );
 CREATE TABLE PRISON_BLOCK(
     blockID INTEGER PRIMARY KEY NOT NULL,
@@ -100,6 +59,52 @@ CREATE TABLE INMATE(
     inCell INTEGER NOT NULL,
     FOREIGN KEY (inCell) REFERENCES CELL(cellNumber)
 );
+CREATE TABLE VISIT(
+
+    visitDate DATE,
+    visitorId INTEGER,
+    relationToInmate TEXT,
+    PRIMARY KEY (visitDate,visitorId)
+
+);
+CREATE TABLE ASSIGNED_DOCTOR(
+    staffID INTEGER,
+    blockID INTEGER,
+
+    FOREIGN KEY (staffId) REFERENCES DOCTOR(staffId),    
+    FOREIGN KEY (blockId) REFERENCES PRISON_BLOCK(blockId)
+   
+);
+CREATE TABLE MEDICAL_APPOINTMENT(
+    appointmentDate DATE,
+    reason TEXT,
+    doctorId INTEGER,
+    inmateId INTEGER PRIMARY KEY NOT NULL,
+    FOREIGN KEY (staffId) REFERENCES DOCTOR(doctorId),
+    FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId)
+);
+CREATE TABLE SENTENCE(
+    inmateId INTEGER,
+    crimeId INTEGER,
+    
+    duration INTEGER,
+    startingDate DATE,
+    --releaseDate AS startingDate + duration DATE,
+
+    FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId),
+    FOREIGN KEY (crimeId) REFERENCES CRIME(crimeId)
+
+    /*severity CASE
+        WHEN duration<=5 THEN 1
+        WHEN duration>5 AND duration<=10 THEN 2
+        WHEN duration>10 AND duration<=20 THEN 3
+        WHEN duration>20 AND duration<=40 THEN 4
+        ELSE 5
+    END as INTEGER;
+    */
+    
+
+);
 CREATE TABLE TEAM_SUPERVISOR(
     supervisor INTEGER NOT NULL,
     supervised INTEGER PRIMARY KEY,
@@ -114,5 +119,5 @@ CREATE TABLE VISITS(
 CREATE TABLE CRIME(
     crimeId INTEGER PRIMARY KEY NOT NULL,
     crimeDescription TEXT
-);
+)
 
