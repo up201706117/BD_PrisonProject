0088adcf5939b5b01e9ac9ba70612ec62b1338ac
Guilherme Attianese Pereira da Costa <up201709560@up.pt>
2021-12-10 17:17:43 +0000

    updated criar.sql

--- criar.sql
+++ criar.sql
@@ -1,35 +1,34 @@
 CREATE TABLE VISIT(
 
-    DATE date
-    INTEGER visitorId
-    TEXT relationToInmate
-    PRIMARY KEY (date)
+    DATE date,
+    INTEGER visitorId,
+    TEXT relationToInmate,
+    PRIMARY KEY (date),
     PRIMARY KEY (visitorId)
 
 )
 
 CREATE TABLE ASSIGNED_DOCTOR (
-    FOREIGN KEY (staffID) REFERENCES DOCTOR(staffID)
-    
+    FOREIGN KEY (staffID) REFERENCES DOCTOR(staffID),    
     FOREIGN KEY (blockID) REFERENCES BLOCK(blockID)
    
 );
 
 CREATE TABLE MEDICAL_APPOINTMENT (
-    DATE date
-    TEXT reason
-    FOREIGN KEY (staffID) REFERENCES DOCTOR(staffID)
+    DATE date,
+    TEXT reason,
+    FOREIGN KEY (staffID) REFERENCES DOCTOR(staffID),
     FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId)
-)
+);
 
 
 CREATE TABLE SENTENCE (
 
-    FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId)
-    FOREIGN KEY (crimeId) REFERENCES CRIME(crimeId)
-    INTEGER duration AS years
-    DATE startingDate
-    DATE releaseDate AS startingDate + duration
+    FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId),
+    FOREIGN KEY (crimeId) REFERENCES CRIME(crimeId),
+    INTEGER duration AS years,
+    DATE startingDate,
+    DATE releaseDate AS startingDate + duration,
 
     INTEGER severity CASE
         WHEN duration<=5 THEN 1
@@ -39,4 +38,4 @@ CREATE TABLE SENTENCE (
         ELSE 5
     END;
 
-)
+);
