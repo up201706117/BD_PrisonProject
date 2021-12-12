0ccac217125d3791388f6c1e6b5a474cc3787ddc
Guilherme Costa <gapc2007@gmail.com>
2021-12-12 22:52:41 +0000

    Added some Constraints to criar.sql, fixed Orto errors, Created Prison.db

--- criar.sql
+++ criar.sql
@@ -11,17 +11,19 @@ DROP TABLE IF EXISTS INMATE;
 DROP TABLE IF EXISTS TEAM_SUPERVISOR;
 DROP TABLE IF EXISTS VISITS;
 DROP TABLE IF EXISTS CRIME;
-DROP TABLE IF EXISTS DOCOTR;
+DROP TABLE IF EXISTS DOCTOR;
+
 CREATE TABLE STAFF(
     staffId INTEGER PRIMARY KEY NOT NULL,
-    staffName TEXT,
-    birthDate DATE
-    --age AS CURDATE() - birthDate,
+    staffName TEXT NOT NULL,
+    birthDate DATE NOT NULL
+    --age AS CURDATE() - birthDate
+    --CHECK (age>=18)
     
 );
 CREATE TABLE GUARD(
     guardId INTEGER PRIMARY KEY NOT NULL,
-    startingDate DATE,
+    startingDate DATE NOT NULL,
     --experience INTEGER,
     assignedTo INTEGER,
     FOREIGN KEY (guardId) REFERENCES STAFF(staffId),
@@ -30,17 +32,19 @@ CREATE TABLE GUARD(
 CREATE TABLE DOCTOR(
     doctorId INTEGER PRIMARY KEY NOT NULL,
     licenseNumber INTEGER UNIQUE NOT NULL,
-    speciality TEXT,
+    specialty TEXT,
     FOREIGN KEY (doctorId) REFERENCES STAFF(staffId)
 );
 CREATE TABLE PRISON_BLOCK(
     blockID INTEGER PRIMARY KEY NOT NULL,
-    supervisor INTEGER NOT NULL UNIQUE,
-    FOREIGN KEY (supervisor) REFERENCES GUARD(guardId)
+    supervisor INTEGER UNIQUE,
+    FOREIGN KEY (supervisor) REFERENCES GUARD(guardId),
+
+    CHECK (supervisor == 1)
 );
 CREATE TABLE CORRIDOR(
     corridorId INTEGER PRIMARY KEY NOT NULL,
-    --numOfCells INTEGER NOT NULL,
+    --numOfCells INTEGER, --DERIVES FROM LAYOUT
     layout INTEGER NOT NULL,
     blockId INTEGER NOT NULL,
     FOREIGN KEY (blockId) REFERENCES PRISON_BLOCK(blockId)
@@ -48,48 +52,49 @@ CREATE TABLE CORRIDOR(
 CREATE TABLE CELL(
     cellNumber INTEGER PRIMARY KEY NOT NULL,
     cellType INTEGER NOT NULL,
-    --capacity INTEGER NOT NULL,
+    --capacity INTEGER NOT NULL, --DERIVES FROM CELLTYPE
     corridorId INTEGER NOT NULL,
     FOREIGN KEY (corridorId) REFERENCES CORRIDOR(corridorId)
 );
 CREATE TABLE INMATE(
     inmateId INTEGER PRIMARY KEY NOT NULL,
-    inmateName TEXT,
-    birthDate DATE,
+    inmateName TEXT NOT NULL,
+    birthDate DATE NOT NULL,
     --age INTEGER AS CURDATE()-birthDate,
-    inCell INTEGER NOT NULL,
+    --CHECK (age>=18),
+    inCell INTEGER,
     FOREIGN KEY (inCell) REFERENCES CELL(cellNumber)
 );
 CREATE TABLE VISIT(
 
-    visitDate DATE,
-    visitorId INTEGER,
+    visitDate DATE NOT NULL,
+    visitorId INTEGER NOT NULL,
     relationToInmate TEXT,
     PRIMARY KEY (visitDate,visitorId)
 
 );
 CREATE TABLE ASSIGNED_DOCTOR(
-    staffID INTEGER,
-    blockID INTEGER,
+    staffID INTEGER NOT NULL,
+    blockID INTEGER NOT NULL,
 
     FOREIGN KEY (staffId) REFERENCES DOCTOR(staffId),    
     FOREIGN KEY (blockId) REFERENCES PRISON_BLOCK(blockId)
    
 );
 CREATE TABLE MEDICAL_APPOINTMENT(
-    appointmentDate DATE,
+    appointmentDate DATE NOT NULL,
     reason TEXT,
-    doctorId INTEGER,
+    doctorId INTEGER NOT NULL,
     inmateId INTEGER PRIMARY KEY NOT NULL,
     FOREIGN KEY (doctorId) REFERENCES DOCTOR(doctorId),
     FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId)
 );
 CREATE TABLE SENTENCE(
-    inmateId INTEGER,
-    crimeId INTEGER,
+    inmateId INTEGER NOT NULL,
+    crimeId INTEGER NOT NULL,
     
-    duration INTEGER,
-    startingDate DATE,
+    duration INTEGER NOT NULL,
+    startingDate DATE NOT NULL,
     --releaseDate AS startingDate + duration DATE,
 
     FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId),
@@ -108,7 +113,7 @@ CREATE TABLE SENTENCE(
 );
 CREATE TABLE TEAM_SUPERVISOR(
     supervisor INTEGER NOT NULL,
-    supervised INTEGER PRIMARY KEY,
+    supervised INTEGER PRIMARY KEY NOT NULL,
     FOREIGN KEY (supervised) REFERENCES GUARD (guardId)
 );
 CREATE TABLE VISITS(
@@ -116,9 +121,13 @@ CREATE TABLE VISITS(
     inmateId INTEGER NOT NULL,
     FOREIGN KEY (visitorId) REFERENCES VISIT(visitorId),
     FOREIGN KEY (inmateId) REFERENCES INMATE(inmateId)
+
+    -- TODO implement visitor/inmate limit = 1
+ 
 );
 CREATE TABLE CRIME(
     crimeId INTEGER PRIMARY KEY NOT NULL,
     crimeDescription TEXT
+    CHECK (length(crimeDescription) >= 3 AND length(crimeDescription) <= 180) 
 );
 
